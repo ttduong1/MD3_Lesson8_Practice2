811ee12139273201ca9e8178dff00ee49e845799
duongtt1 <trinhtungduong2019@gmail.com>
2023-03-13 16:39:41 +0700

    update

---
+++ src/rikkei/academy/test/FizzBuzzTest.java
@@ -0,0 +1,62 @@
+package rikkei.academy.test;
+
+import org.junit.jupiter.api.Test;
+import rikkei.academy.FizzBuzz;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+    @Test
+    void testFizz3(){
+        int number = 3;
+        String expected = "Fizz";
+
+        String result = FizzBuzz.fizzBuzz(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testFizz6(){
+        int number = 6;
+        String expected = "Fizz";
+
+        String result = FizzBuzz.fizzBuzz(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testBuzz5(){
+        int number = 5;
+        String expected = "Buzz";
+
+        String result = FizzBuzz.fizzBuzz(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testBuzz10(){
+        int number = 10;
+        String expected = "Buzz";
+
+        String result = FizzBuzz.fizzBuzz(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testBuzz15(){
+        int number = 15;
+        String expected = "Buzz";
+
+        String result = FizzBuzz.fizzBuzz(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testBuzz30(){
+        int number = 30;
+        String expected = "Buzz";
+
+        String result = FizzBuzz.fizzBuzz(number);
+        assertEquals(expected, result);
+    }
+}
